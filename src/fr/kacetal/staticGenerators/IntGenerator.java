e9e638fd4b9c7c2f4a2128d7c78e5ea452c2ef64
Artem <kackhotali@gmail.com>
2018-04-27 19:20:52 +0200

    effacé des parenthèse inutiles

--- src/fr/kacetal/staticGenerators/IntGenerator.java
+++ src/fr/kacetal/staticGenerators/IntGenerator.java
@@ -3,11 +3,11 @@ package fr.kacetal.staticGenerators;
 public class IntGenerator {
 
     public static int generateInt(final int min, final int max) { // return [min,max)
-        return ((int) ((Math.random() * (max - min)) + min));
+        return (int) Math.random() * (max - min) + min;
     }
 
     public static int generateInt(final int max) { //return [0,max)
-        return ((int) ( Math.random() * max));
+        return (int) Math.random() * max;
     }
     
 }
